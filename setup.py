e6125c9c97d22e6d9d984ae9f6a9818ba1a2d50e
Chris Lalancette <clalancette@gmail.com>
2023-02-14 16:08:59 +0000

    Rebase from 'debian/rolling/rqt_srv'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_srv'
 
 setup(
     name=package_name,
-    version='1.1.0',
+    version='1.1.1',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
@@ -16,8 +16,8 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     author='Aaron Blasdel',
-    maintainer='Dirk Thomas, Aaron Blasdel, Michael Lautman',
-    maintainer_email='dthomas@osrfoundation.org',
+    maintainer='Geoffrey Biggs',
+    maintainer_email='geoff@openrobotics.org',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
